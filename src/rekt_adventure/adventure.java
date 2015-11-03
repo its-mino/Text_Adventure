643163f89218895e29f6c808125153e2953b2321
Nick_Orr <orrn@wit.edu>
2015-11-03 10:00:59 -0500

    >Added text

--- src/rekt_adventure/adventure.java
+++ src/rekt_adventure/adventure.java
@@ -1,10 +1,22 @@
+
+
+
 package rekt_adventure;
 
 public class adventure {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.fuck();
+
+	System.out.println("Welcome to Rekt Adventure, created by Brendan Sileo and Nick Orr");
+	System.out.print("You begin your journey as you approach a dreary cave. Bats fly out ");
+	System.out.print("right as you approach the mouth of the cave. Do you enter? (y/n)");
+	//TODO Allow user to not enter
+	
+	
+		
+		
+		
 	}
 
 }
