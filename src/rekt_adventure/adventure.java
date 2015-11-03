c76bfd5746d1511caae8a09a09bee1fa45828ed0
Nick_Orr <orrn@wit.edu>
2015-11-03 10:15:25 -0500

    >Added game text

--- src/rekt_adventure/adventure.java
+++ src/rekt_adventure/adventure.java
@@ -8,12 +8,20 @@ public class adventure {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	System.out.println("Welcome to Rekt Adventure, created by Brendan Sileo and Nick Orr");
+	System.out.println("Welcome to Rekt Adventure, created by Brendan Sileo and Nick Orr.");
+	System.out.println("");
 	System.out.print("You begin your journey as you approach a dreary cave. Bats fly out ");
-	System.out.print("right as you approach the mouth of the cave. Do you enter? (y/n)");
+	System.out.println("as you stand at the mouth of the cave, giving an ominous effect. ");
+	System.out.println("Do you enter? (y/n)");
+	System.out.println();
 	//TODO Allow user to not enter
 	
-	
+	System.out.println("As you enter the cave, you take inventory of what you have...");
+	System.out.println();
+	System.out.println("A sword, a gun, four bullets and a rope. (view this again by typing inventory)");
+	System.out.println();
+	System.out.print("You enter a split in the cave. You hear screams from the left and see a distant light to the right.");
+	System.out.print("Which way do you want to go?  (L/R)");
 		
 		
 		
